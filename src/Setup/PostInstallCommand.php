fa3940a6671ea1b7b5c5bcce11d0bc7a974e91c4
Steve Kluck <stevekluck@quickenloans.com>
2014-06-18 14:04:46 -0400

    Overwrite finalized composer.json

--- src/Setup/PostInstallCommand.php
+++ src/Setup/PostInstallCommand.php
@@ -12,6 +12,9 @@ use Composer\Script\Event;
 
 class PostInstallCommand
 {
+    const DEFAULT_NAMESPACE = 'QL\SampleApplication';
+    const DEFAULT_PACKAGE_NAME = 'ql/sample-application';
+
     /**
      * The root directory of this package after it is installed.
      *
@@ -38,28 +41,25 @@ class PostInstallCommand
 
     /**
      * @param Event $event
+     *
      * @return null
      */
     public function __invoke(Event $event)
     {
-        $composer = $event->getComposer();
         $io = $event->getIO();
-
-        $io->write('Root: ' . $this->root);
-        $io->write('Application Root: ' . $this->appRoot);
 
         // app settings
         $namespace = $this->getApplicationNamespace($io);
 
         $this->copyConfiguration($io);
-        $this->sanitizeDists($io, $namespace);
+        $this->prepareDists($io, $namespace);
 
-        // overwrite composer.json
-        // ask user for package name
+        $this->prepareComposerConfiguration($io, $namespace);
     }
 
     /**
      * @param Event $event
+     *
      * @return null
      */
     public static function derp(Event $event)
@@ -75,6 +75,7 @@ class PostInstallCommand
      * Copy required configuration to the application.
      *
      * @param IOInterface $io
+     *
      * @return null
      */
     private function copyConfiguration(IOInterface $io)
@@ -94,42 +95,71 @@ class PostInstallCommand
      * Get the namespace for the project being created.
      *
      * @param IOInterface $io
+     *
      * @return null
      */
     private function getApplicationNamespace(IOInterface $io)
     {
-        $io->write('Please enter the namespace of your application:');
+        $io->write('Please enter the namespace of your application.');
         $io->write('Examples: "QL\SampleApplication", "QL\SubNamespace\Example"');
 
-        $namespace = $io->ask('Application namespace: ', 'QL\SampleApplication');
+        $namespace = $io->ask('Application namespace: ', self::DEFAULT_NAMESPACE);
         return rtrim($namespace, '\\');
     }
 
     /**
      * @param IOInterface $io
      * @param string $namespace
+     *
      * @return null
      */
-    private function sanitizeDists(IOInterface $io, $namespace)
+    private function prepareComposerConfiguration(IOInterface $io, $namespace)
     {
-        $io->write('Preparing bin/dump-di');
-        $this->sanitizeDistFile('bin/dump-di', $namespace);
+        $filename = $this->appRoot. '/composer.json';
 
-        $io->write('Preparing configuration/bootstrap.php');
-        $this->sanitizeDistFile('configuration/bootstrap.php', $namespace);
+        $io->write('Please enter the package name of your application.');
+        $io->write('Example: "ql/sample-application"');
 
-        $io->write('Preparing public/index.php');
-        $this->sanitizeDistFile('public/index.php', $namespace);
+        $packageName = $io->ask('Application package name: ', self::DEFAULT_PACKAGE_NAME);
+
+        $json = file_get_contents($filename);
+        $data = json_decode($json);
+
+        $data['name'] = $packageName;
+        $data['description'] = '';
+        $data['autoload']['psr-4'] = [$namespace => 'src'];
+        unset($data['scripts']);
+
+        file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT));
     }
 
     /**
-     * Sanitize dist file.
+     * @param IOInterface $io
+     * @param string $namespace
+     *
+     * @return null
+     */
+    private function prepareDists(IOInterface $io, $namespace)
+    {
+        $io->write('Preparing bin/dump-di');
+        $this->prepareDistFile('bin/dump-di', $namespace);
+
+        $io->write('Preparing configuration/bootstrap.php');
+        $this->prepareDistFile('configuration/bootstrap.php', $namespace);
+
+        $io->write('Preparing public/index.php');
+        $this->prepareDistFile('public/index.php', $namespace);
+    }
+
+    /**
+     * Prepare dist file.
      *
      * @param string $filename
      * @param string $namespace
+     *
      * @return null
      */
-    private function sanitizeDistFile($filename, $namespace)
+    private function prepareDistFile($filename, $namespace)
     {
         $targetFilename = $this->appRoot. '/' . $filename;
         $distFilename = $targetFilename . '.dist';
