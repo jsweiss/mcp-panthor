a911166fb11aeb5f90c0f6f1240c455c7758cabe
Steve Kluck <stevekluck@quickenloans.com>
2014-06-18 13:41:52 -0400

    Make bootstrap and index distable

--- src/Setup/PostInstallCommand.php
+++ src/Setup/PostInstallCommand.php
@@ -11,45 +11,104 @@ use Composer\Script\Event;
 
 class PostInstallCommand
 {
-    public static function derp(Event $event)
-    {
-        $root = __DIR__ . '/../../';
+    /**
+     * The root directory of this package after it is installed.
+     *
+     * @type string
+     */
+    private $root;
 
+    /**
+     * The root directory of the base application.
+     *
+     * @type string
+     */
+    private $appRoot;
+
+    /**
+     * @param string $root
+     * @param string $appRoot
+     */
+    public function __construct($root, $appRoot)
+    {
+        $this->root = $root;
+        $this->appRoot = $appRoot;
+    }
+
+    /**
+     * @param Event $event
+     * @return null
+     */
+    public function __invoke(Event $event)
+    {
         $composer = $event->getComposer();
-        $package = $composer->getPackage();
-        $appRoot = $composer->getInstallationManager()->getInstallPath($package);
         $io = $event->getIO();
+
+        $io->write('Root: ' . $this->root);
+        $io->write('Application Root: ' . $this->appRoot);
 
         // app settings
         // ask IO for this
-        $namespace = 'QL\DerptownUSA';
+        $namespace = $io->ask('Please enter the namespace of your application.',  'QL\SampleApplication');
 
+        $this->copyConfiguration();
 
-        $cmdBin = sprintf('cp -R -v "%s/bin" "%s/bin"', $root, $appRoot);
-        $cmdConfig = sprintf('cp -R -v "%s/configuration" "%s/configuration"', $root, $appRoot);
-        $cmdPublic = sprintf('cp -R -v "%s/public" "%s/public"', $root, $appRoot);
+        $this->sanitizeDistFile('bin/dump-di', $namespace);
+        $this->sanitizeDistFile('configuration/bootstrap.php', $namespace);
+        $this->sanitizeDistFile('public/index.php', $namespace);
+
+        // overwrite composer.json
+        // ask user for package name
+    }
+
+    /**
+     * @param Event $event
+     * @return null
+     */
+    public static function derp(Event $event)
+    {
+        $root = __DIR__ . '/../..';
+        $appRoot = getcwd();
+
+        $command = new static($root, $appRoot);
+        call_user_func($command, $event);
+    }
+
+    /**
+     * Copy required configuration to the application.
+     *
+     * @return null
+     */
+    private function copyConfiguration()
+    {
+        $cmdBin = sprintf('cp -R -v "%s/bin" "%s/bin"', $this->root, $this->appRoot);
+        $cmdConfig = sprintf('cp -R -v "%s/configuration" "%s/configuration"', $this->root, $this->appRoot);
+        $cmdPublic = sprintf('cp -R -v "%s/public" "%s/public"', $this->root, $this->appRoot);
 
         // copy bin/, configuration/, public/
         $io->write('Copying application files');
         exec($cmdBin);
         exec($cmdConfig);
         exec($cmdPublic);
+    }
 
-        // sanitize these files:
+    /**
+     * Sanitize dist file.
+     *
+     * @param string $filename
+     * @param string $namespace
+     * @return null
+     */
+    private function sanitizeDistFile($filename, $namespace)
+    {
+        $io->write('Preparing ' . $filename);
+        $targetFilename = $this->appRoot. '/' . $filename;
+        $distFilename = $targetFilename . '.dist';
 
-        // bin/dump-di.dist -> put it at bin/dump-di
-        $io->write('Preparing bin/dump-di.dist');
-        $dumpDi = file_get_contents($appRoot. '/bin/dump-di.dist');
-        $dumpDi = str_replace('{{ application.namespace }}', $namespace);
-        file_put_contents($appRoot. '/bin/dump-di', $dumpDi);
-        unlink($appRoot. '/bin/dump-di.dist');
+        $contents = file_get_contents($distFilename);
+        $contents = str_replace('{{ application.namespace }}', $namespace, $contents);
 
-        // configuration/bootstrap.php.dist -> put it at configuration/bootstrap.php
-        // public/index.php.dist -> put it at public/index.php
-
-        // overwrite composer.json
-        // ask user for package name
-
-        $io->write(var_export($root, true));
+        file_put_contents($targetFilename, $contents);
+        unlink($distFilename);
     }
 }
